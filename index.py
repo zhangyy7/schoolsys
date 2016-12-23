4c5a4d82811763a71a11f54f995597e36610e43b
zhangyy <m_zhangyinyue@163.com>
2016-12-24 01:18:36 +0800

    20161224.2

--- index.py
+++ index.py
@@ -1,16 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import bin.main
-import util
-import settings
 
 
 def main():
     bin.main.main()
-    # school_dict = util.upickle_from_file(
-    #     settings.DATABASE["engineer"]["file"]["school"])
-    # for num, school in school_dict.items():
-    #     print(num, school.name)
 
 
 if __name__ == '__main__':
